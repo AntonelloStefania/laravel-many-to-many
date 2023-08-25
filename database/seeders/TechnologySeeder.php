e9808acf37a617bb079aba92ef7359461b19dbdf
AntonelloStefania <antonelloste94@gmail.com>
2023-08-25 18:13:02 +0200

    creata tabella technologies e seeder technology(da lanciare)

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Technology;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies=['HTML', 'CSS', 'Javascript', 'JQuery', 'VueJS', 'Laravel', 'Symfony','SASS'];
+
+        foreach($technologies as $tech){
+            $new_tech = new Technology();
+            $new_tech->tech_name=$tech;
+        }
+    }
+}
